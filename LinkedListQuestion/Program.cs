e883708d68012612a396a18c192837a1bbd99996
Yakov Pizenberg <thelittlecitizen17@gmail.com>
2020-08-11 23:56:28 +0300

    Adding the final answer

---
+++ LinkedListQuestion/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinkedListQuestion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<int> linkedList = new LinkedList<int>();
+            linkedList.AddLast(1);
+            linkedList.AddLast(2);
+            linkedList.AddLast(3);
+            linkedList.AddLast(4);
+            linkedList.AddLast(5);
+            linkedList.AddLast(6);
+
+            bool isCircular = IsCircular(linkedList);
+            Console.WriteLine(isCircular);
+
+        }
+        static bool IsCircular<T>(LinkedList<T> linkedList)
+        {
+            var slow = linkedList.First.Next;
+            var fast = linkedList.First.Next.Next;
+
+            while (slow != null && fast != null)
+            {
+
+
+                if (slow == fast)
+                {
+                    return true;
+                }
+                if (fast.Next != null)
+                {
+                    fast = fast.Next.Next;
+                }
+                slow = slow.Next;
+            }
+            return false;
+        }
+    }
+}
+
